6b9d39dfa5a7acc448c8a081997ce634414e5337
Payel <payelmarjit2007@gmail.com>
2026-09-26 23:13:40 +0530

    commit array

---
+++ If Else/check_leap_year_or_not.c
@@ -0,0 +1,12 @@
+//WAP to check a year leap year or not 
+#include<stdio.h>
+void main()
+{
+    int x;
+    printf("enter a year.=");
+    scanf("%d",&x);
+    if ((x%400==0)||((x%4==7) && (x%100!=0)))
+      printf("%d is a leap year",x);
+    else
+      printf("%d is not a leap year",x);
+}                                                                                                                                
---
+++ If Else/ph_bill_calculation.c
@@ -0,0 +1,19 @@
+//WAP to calculate phone Bill Calc.
+#include<stdio.h>
+void main()
+{
+    int unit,rs;
+    printf("Enter the Unit = ");
+    scanf("%d",&unit);
+    if(unit>=0 && unit<=100)
+      rs=unit*1;
+    else if(unit>100 && unit<=200)
+      rs=(unit-100)*4+100;
+    else if(unit>200 && unit<=400)
+      rs=(unit-200)*8+500;
+    else if(unit>400 && unit<=800)
+     rs=(unit-400)*10+2100;
+    else
+      rs=(unit-800)*12+6100;
+    printf("Your Bill Amount = %d",rs);
+}                                                                                                                                                          
